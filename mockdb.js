9b70d0f19d7747c8c7922d3314ad41bfadd8ae26
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2019-06-06 21:08:42 -0400

    enable cors in mockdb

--- mockdb.js
+++ mockdb.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 let app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 let afl = [
   { 'level': '1', 'usernames': ['birnbaum'] },
