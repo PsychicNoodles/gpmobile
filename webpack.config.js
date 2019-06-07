ba31589e1e8eece0d8f0f04ad2e82b31c93eabe2
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2019-06-06 21:00:56 -0400

    added eslint and express

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         options: {
           loaders: {
             'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
         }
       },
